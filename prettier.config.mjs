985b41e1d4ff6704a116b6d9f89e6ee4fb26d963
Kaiming Guo <me@kaiming.io>
2026-02-23 05:15:43 +0800

    Initial commit

---
+++ prettier.config.mjs
@@ -0,0 +1,21 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+    trailingComma: "es5",
+    tabWidth: 4,
+    semi: true,
+    singleQuote: false,
+    plugins: [import("prettier-plugin-astro")],
+    overrides: [
+        {
+            files: "*.astro",
+            options: {
+                parser: "astro",
+            },
+        },
+    ],
+};
+
+export default config;
