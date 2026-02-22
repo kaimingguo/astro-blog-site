5e27e20ac99cbebb56930cd7a88d746b33022822
Kaiming Guo <me@kaiming.io>
2026-02-23 05:46:10 +0800

    add Matrix client and server configuration files and update headers

--- src/pages/.well-known/matrix/client.ts
+++
@@ -1,9 +0,0 @@
-import type { APIRoute } from "astro";
-
-export const GET: APIRoute = (ctx) => {
-	const { DOMAIN } = ctx.locals.runtime.env;
-	return new Response(
-		JSON.stringify({ "m.homeserver": { base_url: `https://matrix.${DOMAIN}` } }),
-		{ headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" } },
-	);
-};
--- src/pages/.well-known/matrix/server.ts
+++
@@ -1,9 +0,0 @@
-import type { APIRoute } from "astro";
-
-export const GET: APIRoute = (ctx) => {
-	const { DOMAIN } = ctx.locals.runtime.env;
-	return new Response(
-		JSON.stringify({ "m.server": `matrix.${DOMAIN}:443` }),
-		{ headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" } },
-	);
-};
